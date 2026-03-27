c706842fd0d42d2024be24d2e605c21b32a12037
Ahmad Rizhan <79746517+ahmad-rizhan@users.noreply.github.com>
2026-03-27 10:58:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import joblib
@@ -6,10 +7,9 @@ import joblib
 model = joblib.load('model.joblib')
 
 st.title("Prediksi Dropout Mahasiswa")
-
 st.write("Masukkan data mahasiswa:")
 
-# Input user
+# INPUT USER
 age = st.number_input("Age at Enrollment", 15, 70, 20)
 
 debtor_label = st.selectbox(
@@ -24,25 +24,58 @@ tuition_label = st.selectbox(
 )
 tuition = 0 if tuition_label == "Belum Lunas" else 1
 
-grade1 = st.number_input("Nilai Semester 1", 0.0, 20.0, 10.0)
-grade2 = st.number_input("Nilai Semester 2", 0.0, 20.0, 10.0)
+scholarship_label = st.selectbox(
+    "Penerima Beasiswa",
+    ["Tidak", "Ya"]
+)
+scholarship = 1 if scholarship_label == "Ya" else 0
 
-# Tombol prediksi
+gender_label = st.selectbox(
+    "Jenis Kelamin",
+    ["Perempuan", "Laki-laki"]
+)
+gender = 1 if gender_label == "Laki-laki" else 0
+
+admission_grade = st.number_input("Nilai Masuk", 0.0, 20.0, 10.0)
+
+grade1 = st.number_input("Nilai Semester 1", 0.0, 20.0, 10.0)
+approved1 = st.number_input("Jumlah Matkul Lulus Semester 1", 0, 20, 5)
+
+grade2 = st.number_input("Nilai Semester 2", 0.0, 20.0, 10.0)
+approved2 = st.number_input("Jumlah Matkul Lulus Semester 2", 0, 20, 5)
+
+# PREDICT
 if st.button("Predict"):
     input_data = pd.DataFrame(
-        [[age, debtor, tuition, grade1, grade2]],
+        [[
+            age,
+            debtor,
+            tuition,
+            scholarship,
+            gender,
+            admission_grade,
+            grade1,
+            approved1,
+            grade2,
+            approved2
+        ]],
         columns=[
             'Age_at_enrollment',
             'Debtor',
             'Tuition_fees_up_to_date',
+            'Scholarship_holder',
+            'Gender',
+            'Admission_grade',
             'Curricular_units_1st_sem_grade',
-            'Curricular_units_2nd_sem_grade'
+            'Curricular_units_1st_sem_approved',
+            'Curricular_units_2nd_sem_grade',
+            'Curricular_units_2nd_sem_approved'
         ]
     )
 
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
-        st.error("Mahasiswa berpotensi DROPOUT")
-    else:
         st.success("Mahasiswa berpotensi LULUS")
+    else:
+        st.error("Mahasiswa berpotensi DROPOUT")
